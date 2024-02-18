79a2dc4ed54b1d46f34b90d468b3aabcd1084b79
Peter Kalverla <peter.kalverla@gmx.com>
2024-02-18 22:11:27 +0100

    Set default config from example run

--- src/classmodel/config.py
+++ src/classmodel/config.py
@@ -4,111 +4,111 @@ from dataclasses import dataclass
 
 
 @dataclass
-class ModelInput:
+class CLASSConfig:
     """Class for storing mixed-layer model input data."""
 
     # general model variables
-    runtime = None  # duration of model run [s]
-    dt = None  # time step [s]
+    runtime = 12 * 3600  # total run time [s]
+    dt = 60.0  # time step [s]
 
     # mixed-layer variables
-    sw_ml = None  # mixed-layer model switch
-    sw_shearwe = None  # Shear growth ABL switch
-    sw_fixft = None  # Fix the free-troposphere switch
-    h = None  # initial ABL height [m]
-    Ps = None  # surface pressure [Pa]
-    divU = None  # horizontal large-scale divergence of wind [s-1]
-    fc = None  # Coriolis parameter [s-1]
+    sw_ml = True  # mixed-layer model switch
+    sw_shearwe = False  # Shear growth ABL switch
+    sw_fixft = False  # Fix the free-troposphere switch
+    h = 200.0  # initial ABL height [m]
+    Ps = 101300.0  # surface pressure [Pa]
+    divU = 0.0  # horizontal large-scale divergence of wind [s-1]
+    fc = 1.0e-4  # Coriolis parameter [s-1]
 
-    theta = None  # initial mixed-layer potential temperature [K]
-    dtheta = None  # initial temperature jump at h [K]
-    gammatheta = None  # free atmosphere potential temperature lapse rate [K m-1]
-    advtheta = None  # advection of heat [K s-1]
-    beta = None  # entrainment ratio for virtual heat [-]
-    wtheta = None  # surface kinematic heat flux [K m s-1]
+    theta = 288.0  # initial mixed-layer potential temperature [K]
+    dtheta = 1.0  # initial temperature jump at h [K]
+    gammatheta = 0.006  # free atmosphere potential temperature lapse rate [K m-1]
+    advtheta = 0.0  # advection of heat [K s-1]
+    beta = 0.2  # entrainment ratio for virtual heat [-]
+    wtheta = 0.1  # surface kinematic heat flux [K m s-1]
 
-    q = None  # initial mixed-layer specific humidity [kg kg-1]
-    dq = None  # initial specific humidity jump at h [kg kg-1]
-    gammaq = None  # free atmosphere specific humidity lapse rate [kg kg-1 m-1]
-    advq = None  # advection of moisture [kg kg-1 s-1]
-    wq = None  # surface kinematic moisture flux [kg kg-1 m s-1]
+    q = 0.008  # initial mixed-layer specific humidity [kg kg-1]
+    dq = -0.001  # initial specific humidity jump at h [kg kg-1]
+    gammaq = 0.0  # free atmosphere specific humidity lapse rate [kg kg-1 m-1]
+    advq = 0.0  # advection of moisture [kg kg-1 s-1]
+    wq = 0.1e-3  # surface kinematic moisture flux [kg kg-1 m s-1]
 
-    CO2 = None  # initial mixed-layer potential temperature [K]
-    dCO2 = None  # initial temperature jump at h [K]
-    gammaCO2 = None  # free atmosphere potential temperature lapse rate [K m-1]
-    advCO2 = None  # advection of heat [K s-1]
-    wCO2 = None  # surface kinematic heat flux [K m s-1]
+    CO2 = 422.0  # initial mixed-layer potential temperature [K]
+    dCO2 = -44.0  # initial temperature jump at h [K]
+    gammaCO2 = 0.0  # free atmosphere potential temperature lapse rate [K m-1]
+    advCO2 = 0.0  # advection of heat [K s-1]
+    wCO2 = 0.0  # surface kinematic heat flux [K m s-1]
 
-    sw_wind = None  # prognostic wind switch
-    u = None  # initial mixed-layer u-wind speed [m s-1]
-    du = None  # initial u-wind jump at h [m s-1]
-    gammau = None  # free atmosphere u-wind speed lapse rate [s-1]
-    advu = None  # advection of u-wind [m s-2]
+    sw_wind = False  # prognostic wind switch
+    u = 6.0  # initial mixed-layer u-wind speed [m s-1]
+    du = 4.0  # initial u-wind jump at h [m s-1]
+    gammau = 0.0  # free atmosphere u-wind speed lapse rate [s-1]
+    advu = 0.0  # advection of u-wind [m s-2]
 
-    v = None  # initial mixed-layer u-wind speed [m s-1]
-    dv = None  # initial u-wind jump at h [m s-1]
-    gammav = None  # free atmosphere v-wind speed lapse rate [s-1]
-    advv = None  # advection of v-wind [m s-2]
+    v = -4.0  # initial mixed-layer u-wind speed [m s-1]
+    dv = 4.0  # initial u-wind jump at h [m s-1]
+    gammav = 0.0  # free atmosphere v-wind speed lapse rate [s-1]
+    advv = 0.0  # advection of v-wind [m s-2]
 
     # surface layer variables
-    sw_sl = None  # surface layer switch
-    ustar = None  # surface friction velocity [m s-1]
-    z0m = None  # roughness length for momentum [m]
-    z0h = None  # roughness length for scalars [m]
+    sw_sl = False  # surface layer switch
+    ustar = 0.3  # surface friction velocity [m s-1]
+    z0m = 0.02  # roughness length for momentum [m]
+    z0h = 0.002  # roughness length for scalars [m]
     Cm = None  # drag coefficient for momentum [-]
     Cs = None  # drag coefficient for scalars [-]
     L = None  # Obukhov length [-]
     Rib = None  # bulk Richardson number [-]
 
     # radiation parameters
-    sw_rad = None  # radiation switch
-    lat = None  # latitude [deg]
-    lon = None  # longitude [deg]
-    doy = None  # day of the year [-]
-    tstart = None  # time of the day [h UTC]
-    cc = None  # cloud cover fraction [-]
-    Q = None  # net radiation [W m-2]
-    dFz = None  # cloud top radiative divergence [W m-2]
+    sw_rad = False  # radiation switch
+    lat = 51.97  # latitude [deg]
+    lon = -4.93  # longitude [deg]
+    doy = 268.0  # day of the year [-]
+    tstart = 6.8  # time of the day [h UTC]
+    cc = 0.0  # cloud cover fraction [-]
+    Q = 400.0  # net radiation [W m-2]
+    dFz = 0.0  # cloud top radiative divergence [W m-2]
 
     # land surface parameters
-    sw_ls = None  # land surface switch
-    ls_type = None  # land-surface parameterization ('js' for Jarvis-Stewart or 'ags' for A-Gs)
-    wg = None  # volumetric water content top soil layer [m3 m-3]
-    w2 = None  # volumetric water content deeper soil layer [m3 m-3]
-    Tsoil = None  # temperature top soil layer [K]
-    T2 = None  # temperature deeper soil layer [K]
+    sw_ls = False  # land surface switch
+    ls_type = "js"  # land-surface parameterization ('js' for Jarvis-Stewart or 'ags' for A-Gs)
+    wg = 0.21  # volumetric water content top soil layer [m3 m-3]
+    w2 = 0.21  # volumetric water content deeper soil layer [m3 m-3]
+    Tsoil = 285.0  # temperature top soil layer [K]
+    T2 = 286.0  # temperature deeper soil layer [K]
 
-    a = None  # Clapp and Hornberger retention curve parameter a
-    b = None  # Clapp and Hornberger retention curve parameter b
-    p = None  # Clapp and Hornberger retention curve parameter p
-    CGsat = None  # saturated soil conductivity for heat
+    a = 0.219  # Clapp and Hornberger retention curve parameter a
+    b = 4.90  # Clapp and Hornberger retention curve parameter b
+    p = 4.0  # Clapp and Hornberger retention curve parameter c
+    CGsat = 3.56e-6  # saturated soil conductivity for heat
 
-    wsat = None  # saturated volumetric water content ECMWF config [-]
-    wfc = None  # volumetric water content field capacity [-]
-    wwilt = None  # volumetric water content wilting point [-]
+    wsat = 0.472  # saturated volumetric water content ECMWF config [-]
+    wfc = 0.323  # volumetric water content field capacity [-]
+    wwilt = 0.171  # volumetric water content wilting point [-]
 
-    C1sat = None
-    C2ref = None
+    C1sat = 0.132
+    C2ref = 1.8
 
     c_beta = None  # Curvatur plant water-stress factor (0..1) [-]
 
-    LAI = None  # leaf area index [-]
-    gD = None  # correction factor transpiration for VPD [-]
-    rsmin = None  # minimum resistance transpiration [s m-1]
-    rssoilmin = None  # minimum resistance soil evaporation [s m-1]
-    alpha = None  # surface albedo [-]
+    LAI = 2.0  # leaf area index [-]
+    gD = 0.0  # correction factor transpiration for VPD [-]
+    rsmin = 110.0  # minimum resistance transpiration [s m-1]
+    rssoilmin = 50.0  # minimun resistance soil evaporation [s m-1]
+    alpha = 0.25  # surface albedo [-]
 
-    Ts = None  # initial surface temperature [K]
+    Ts = 290.0  # initial surface temperature [K]
 
-    cveg = None  # vegetation fraction [-]
-    Wmax = None  # thickness of water layer on wet vegetation [m]
-    Wl = None  # equivalent water layer depth for wet vegetation [m]
+    cveg = 0.85  # vegetation fraction [-]
+    Wmax = 0.0002  # thickness of water layer on wet vegetation [m]
+    Wl = 0.0000  # equivalent water layer depth for wet vegetation [m]
 
-    Lambda = None  # thermal diffusivity skin layer [-]
+    Lambda = 5.9  # thermal diffusivity skin layer [-]
 
     # A-Gs parameters
-    c3c4 = None  # Plant type ('c3' or 'c4')
+    c3c4 = "c3"  # Plant type ('c3' or 'c4')
 
     # Cumulus parameters
-    sw_cu = None  # Cumulus parameterization switch
-    dz_h = None  # Transition layer thickness [m]
+    sw_cu = False  # Cumulus parameterization switch
+    dz_h = 150.0  # Transition layer thickness [m]
--- tests/test_model.py
+++ tests/test_model.py
@@ -10,109 +10,10 @@ import sys
 import pandas as pd
 
 from classmodel.model import Model
-from classmodel.config import ModelInput
+from classmodel.config import CLASSConfig
 
 
 REFERENCE_DATA = "tests/test_output.csv"
-
-
-def default_config():
-    config = ModelInput()
-    config.dt = 60.0  # time step [s]
-    config.runtime = 12 * 3600  # total run time [s]
-
-    # mixed-layer input
-    config.sw_ml = True  # mixed-layer model switch
-    config.sw_shearwe = False  # shear growth mixed-layer switch
-    config.sw_fixft = False  # Fix the free-troposphere switch
-    config.h = 200.0  # initial ABL height [m]
-    config.Ps = 101300.0  # surface pressure [Pa]
-    config.divU = 0.0  # horizontal large-scale divergence of wind [s-1]
-    config.fc = 1.0e-4  # Coriolis parameter [m s-1]
-
-    config.theta = 288.0  # initial mixed-layer potential temperature [K]
-    config.dtheta = 1.0  # initial temperature jump at h [K]
-    config.gammatheta = (
-        0.006  # free atmosphere potential temperature lapse rate [K m-1]
-    )
-    config.advtheta = 0.0  # advection of heat [K s-1]
-    config.beta = 0.2  # entrainment ratio for virtual heat [-]
-    config.wtheta = 0.1  # surface kinematic heat flux [K m s-1]
-
-    config.q = 0.008  # initial mixed-layer specific humidity [kg kg-1]
-    config.dq = -0.001  # initial specific humidity jump at h [kg kg-1]
-    config.gammaq = 0.0  # free atmosphere specific humidity lapse rate [kg kg-1 m-1]
-    config.advq = 0.0  # advection of moisture [kg kg-1 s-1]
-    config.wq = 0.1e-3  # surface kinematic moisture flux [kg kg-1 m s-1]
-
-    config.CO2 = 422.0  # initial mixed-layer CO2 [ppm]
-    config.dCO2 = -44.0  # initial CO2 jump at h [ppm]
-    config.gammaCO2 = 0.0  # free atmosphere CO2 lapse rate [ppm m-1]
-    config.advCO2 = 0.0  # advection of CO2 [ppm s-1]
-    config.wCO2 = 0.0  # surface kinematic CO2 flux [ppm m s-1]
-
-    config.sw_wind = False  # prognostic wind switch
-    config.u = 6.0  # initial mixed-layer u-wind speed [m s-1]
-    config.du = 4.0  # initial u-wind jump at h [m s-1]
-    config.gammau = 0.0  # free atmosphere u-wind speed lapse rate [s-1]
-    config.advu = 0.0  # advection of u-wind [m s-2]
-
-    config.v = -4.0  # initial mixed-layer u-wind speed [m s-1]
-    config.dv = 4.0  # initial u-wind jump at h [m s-1]
-    config.gammav = 0.0  # free atmosphere v-wind speed lapse rate [s-1]
-    config.advv = 0.0  # advection of v-wind [m s-2]
-
-    config.sw_sl = False  # surface layer switch
-    config.ustar = 0.3  # surface friction velocity [m s-1]
-    config.z0m = 0.02  # roughness length for momentum [m]
-    config.z0h = 0.002  # roughness length for scalars [m]
-
-    config.sw_rad = False  # radiation switch
-    config.lat = 51.97  # latitude [deg]
-    config.lon = -4.93  # longitude [deg]
-    config.doy = 268.0  # day of the year [-]
-    config.tstart = 6.8  # time of the day [h UTC]
-    config.cc = 0.0  # cloud cover fraction [-]
-    config.Q = 400.0  # net radiation [W m-2]
-    config.dFz = 0.0  # cloud top radiative divergence [W m-2]
-
-    config.sw_ls = False  # land surface switch
-    config.ls_type = "js"  # land-surface parameterization ('js' for Jarvis-Stewart or 'ags' for A-Gs)
-    config.wg = 0.21  # volumetric water content top soil layer [m3 m-3]
-    config.w2 = 0.21  # volumetric water content deeper soil layer [m3 m-3]
-    config.cveg = 0.85  # vegetation fraction [-]
-    config.Tsoil = 285.0  # temperature top soil layer [K]
-    config.T2 = 286.0  # temperature deeper soil layer [K]
-    config.a = 0.219  # Clapp and Hornberger retention curve parameter a
-    config.b = 4.90  # Clapp and Hornberger retention curve parameter b
-    config.p = 4.0  # Clapp and Hornberger retention curve parameter c
-    config.CGsat = 3.56e-6  # saturated soil conductivity for heat
-
-    config.wsat = 0.472  # saturated volumetric water content ECMWF config [-]
-    config.wfc = 0.323  # volumetric water content field capacity [-]
-    config.wwilt = 0.171  # volumetric water content wilting point [-]
-
-    config.C1sat = 0.132
-    config.C2ref = 1.8
-
-    config.LAI = 2.0  # leaf area index [-]
-    config.gD = 0.0  # correction factor transpiration for VPD [-]
-    config.rsmin = 110.0  # minimum resistance transpiration [s m-1]
-    config.rssoilmin = 50.0  # minimun resistance soil evaporation [s m-1]
-    config.alpha = 0.25  # surface albedo [-]
-
-    config.Ts = 290.0  # initial surface temperature [K]
-
-    config.Wmax = 0.0002  # thickness of water layer on wet vegetation [m]
-    config.Wl = 0.0000  # equivalent water layer depth for wet vegetation [m]
-
-    config.Lambda = 5.9  # thermal diffusivity skin layer [-]
-
-    config.c3c4 = "c3"  # Plant type ('c3' or 'c4')
-
-    config.sw_cu = False  # Cumulus parameterization switch
-    config.dz_h = 150.0  # Transition layer thickness [m]
-    return config
 
 
 def update_reference_data():
@@ -120,7 +21,7 @@ def update_reference_data():
 
     Should be run only once when the expected output changes.
     """
-    config = default_config()
+    config = CLASSConfig()
     r1 = Model(config)
     r1.run()
     output = r1.out.to_pandas()
@@ -129,7 +30,7 @@ def update_reference_data():
 
 def test_model():
     """Verify that model with default config reproduces previous result."""
-    config = default_config()
+    config = CLASSConfig()
     r1 = Model(config)
     r1.run()
     output = r1.out.to_pandas()
